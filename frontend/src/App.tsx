a126d3ae48a86147674bc8e004ccd3e406beb448
peterwilfride <cybermind545@gmail.com>
2022-07-13 15:45:51 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButtom from "./components/NotificationButtom"
+
 function App() {
   return(
-    <h1>Olá mundo</h1>
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButtom></NotificationButtom>
+    </>
   )
 }
 
