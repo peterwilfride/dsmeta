b2d17b7e24ba2ec5229e61a7778cfcc53028b573
peterwilfride <cybermind545@gmail.com>
2022-07-13 16:44:35 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import NotificationButtom from "./components/NotificationButtom"
+import Header from './components/Header'
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return(
     <>
-      <h1>Olá mundo</h1>
-      <NotificationButtom></NotificationButtom>
+      <Header></Header>
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard></SalesCard>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
